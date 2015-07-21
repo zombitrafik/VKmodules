a7a94e939ec170594f64a36bcf7148a2fdb2c398
zombitrafik <zombitrafik05@gmail.com>
2015-07-21 09:57:58 +0300

    test

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var vk = new VK({
 
 app.get('/', function (req, res) {
 
+	/*
 	vk.requestServerToken();
 
 	// Waiting for special 'serverTokenReady' event
@@ -19,9 +20,16 @@ app.get('/', function (req, res) {
 	    // Here will be server access token
 	    res.json({data: _o});
 	});
+*/
+res.redirect('https://oauth.vk.com/authorize?client_id=5002560&redirect_uri=https://vkmodules.herokuapp.com/callback&scope=friends,video,offline&display=popup');
 
-    
 });
+
+
+app.get('/callback', function (req, res) {
+	res.json(req);
+});
+
 
 app.set('port', process.env.PORT || 3000);
 
