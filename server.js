7a132b19ca4992a162089ab7307404f77424b65a
zombitrafik <zombitrafik05@gmail.com>
2015-07-21 09:46:16 +0300

    vksdk add

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var VK = require('vksdk');
 
 var app = express();
 
+var vk = new VK({
+    'appID'     : 5002560,
+    'appSecret' : 'DxgrLEDQGv94uODwpY9d',
+    'mode'      : 'oauth'
+});
+
+
 app.get('/', function (req, res) {
-    res.json({data: 'data'});
+
+	vk.requestServerToken();
+
+	// Waiting for special 'serverTokenReady' event
+	vk.on('serverTokenReady', function(_o) {
+	    // Here will be server access token
+	    res.json({data: _o});
+	});
+
+    
 });
 
 app.set('port', process.env.PORT || 3000);
